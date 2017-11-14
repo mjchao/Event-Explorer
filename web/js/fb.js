d080c7a2fd92bb74993f27fff77911527c8a4ada
Mickey Chao <mjchao2@gmail.com>
2017-11-13 22:07:14 -0600

    Get event data over to UI javascript.

--- web/js/fb.js
+++ web/js/fb.js
@@ -1,34 +1,48 @@
 "use strict"
 
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '507641596259132',
-    xfbml      : true,
-    version    : 'v2.11'
-  });
-  FB.AppEvents.logPageView();
-};
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "https://connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
 /**
- * Manages all communicatoins with Facebook.
+ * Manages all communications with Facebook.
  */
 class FbManager {
 
   constructor() {
+
+    // Functions that are deferred until Facebook API is initialized.
+    this.deferredFunctions_ = [];
+
     // The user's login token. It's okay if it's null. null just means the user
     // is not yet logged in.
     this.fbToken = JSON.parse(window.localStorage.getItem("fbToken"));
     if (this.fbToken !== null) {
       this.useAuthToken(this.fbToken);
     }
+  }
+
+  /**
+   * Defers the given function until the Facebook API is initialized.
+   *
+   * @param {function} f A function to defer.
+   */
+  deferUntilConnected(f) {
+    if (typeof FB === "undefined") {
+      this.deferredFunctions_.push(f);
+    }
+    else {
+
+      // Facebook API already initialized.
+      f();
+    }
+  }
+
+  /**
+   * Runs all functions that have been deferred until after the Facebook API
+   * is set up.
+   */
+  runDeferredFunctions() {
+    for (var i = 0; i < this.deferredFunctions_.length; ++i) {
+      this.deferredFunctions_[i]();
+    }
+    this.deferredFunctions_ = [];
   }
 
   /**
@@ -52,8 +66,8 @@ class FbManager {
   /**
    * Gets the user's created events for sharing.
    *
-   * @param {function} Callback that will receive the events when they are
-   *    received from Facebook.
+   * @param {function} callback Callback that will receive the events when they
+   *    are received from Facebook.
    */
   getUserEvents(callback) {
     FB.api(
@@ -63,11 +77,48 @@ class FbManager {
         "access_token": this.fbToken.accessToken,
         "type:": "created"
       },
-      function (response) {
-        console.log(response);
+      function(response) {
+        callback(response);
+      }
+    );
+  }
+
+  /**
+   * Gets the data for a specific event.
+   *
+   * @param {string} eventId The facebook Event ID.
+   * @param {function} callback The callback to execute wtih the event data.
+   */
+  getEvent(eventId, callback) {
+    FB.api(
+      "/" + eventId,
+      "get",
+      {
+        "access_token": this.fbToken.accessToken
+      },
+      function(response) {
+        callback(response);
       }
     );
   }
 }
 
 var FB_MGR = new FbManager();
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '507641596259132',
+    xfbml      : true,
+    version    : 'v2.11'
+  });
+  FB.AppEvents.logPageView();
+  FB_MGR.runDeferredFunctions();
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
--- web/js/share.js
+++ web/js/share.js
@@ -2,3 +2,44 @@
 
 $.getScript("js/fb.js");
 
+class ShareManager {
+
+  constructor() {
+    // The event selector list
+    this.eventList_ = document.querySelector(".event-list");
+  }
+
+  /**
+   * Adds an event to the list of events that the user can choose from.
+   *
+   * @param {object} The event data, e.g. title and time.
+   */
+  addEvent(eventData) {
+    //TODO Add events to list.
+    console.log("Adding event");
+    console.log(eventData);
+    var newEventPanel = $("div", {"class": "event"});
+  }
+
+  /**
+   * Load all of this user's events.
+   */
+  loadEvents() {
+    FB_MGR.getUserEvents(
+      function(events) {
+        for (var i = 0; i < events.data.length; ++i) {
+          SHARE_MGR.addEvent(events.data[i]);
+        }
+      }
+    );
+  }
+}
+
+var SHARE_MGR = new ShareManager();
+$(document).ready(function() {
+  FB_MGR.deferUntilConnected(
+    function() {
+      SHARE_MGR.loadEvents();
+    }
+  );
+});
